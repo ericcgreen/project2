a1edaa4a66b8e6d3003e347073217c02adb31f26
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-01 09:44:07 -0500

    star rating added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,50 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+$("#star-1").click(function(){
+  $(".star-bar").css("width", "20%");
+  $("#star-val").val(1);
+})
+$("#star-2").click(function(){
+  $(".star-bar").css("width", "40%");
+  $("#star-val").val(2);
+})
+$("#star-3").click(function(){
+  $(".star-bar").css("width", "60%");
+  $("#star-val").val(3);
+})
+$("#star-4").click(function(){
+  $(".star-bar").css("width", "80%");
+  $("#star-val").val(4);
+})
+$("#star-5").click(function(){
+  $(".star-bar").css("width", "100%");
+  $("#star-val").val(5);
+})
+
+$("#star-1b").click(function(){
+  $(".star-bartwo").css("width", "20%");
+  $("#star-val2").val(1);
+})
+$("#star-2b").click(function(){
+  $(".star-bartwo").css("width", "40%");
+  $("#star-val2").val(2);
+})
+$("#star-3b").click(function(){
+  $(".star-bartwo").css("width", "60%");
+  $("#star-val2").val(3);
+})
+$("#star-4b").click(function(){
+  $(".star-bartwo").css("width", "80%");
+  $("#star-val2").val(4);
+})
+$("#star-5b").click(function(){
+  $(".star-bartwo").css("width", "100%");
+  $("#star-val2").val(5);
+})
+
+})
